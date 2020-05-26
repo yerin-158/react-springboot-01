693e50655c5bd856f2a2ffaf6f0b9551e3c88a72
yerin-159 <hanblueblue@gmail.com>
2020-05-26 11:30:16 +0900

    Initial commit

---
+++ src/main/java/com/react01/project/api/testController.java
@@ -0,0 +1,15 @@
+package com.react01.project.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+
+    @GetMapping("/")
+    public String test(){
+        return "Hello World!";
+    }
+
+}
